08d7b3850036ece27e84ed9015f9b7141df0647b
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-06-22 02:47:26 -0700

    FInish ajax request

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="row">
-    <div v-for="list in original_lists" :key="list.name" class="col-3">
+    <div v-for="list in lists" :key="list.name" class="col-3">
       <h6>{{ list.name}}</h6>
       <hr />
 
@@ -9,9 +9,9 @@
         {{ card.name}}
       </div>
 
-      <div>
-        <textarea rows="3"></textarea>
-        <button class="btn btn-secondary"></button>
+      <div class="card card-body">
+        <textarea rows="3" class="form-control" v-model='messages[list.id]'></textarea>
+        <button class="btn btn-secondary" @click="addTask(list)">Add Task</button>
       </div>
     </div>
   </div>
@@ -20,6 +20,37 @@
 <script>
 export default {
   props: ["original_lists"],
+  data: function() {
+    return {
+      messages: {},
+      lists: this.original_lists
+    }
+  },
+  methods: {
+    addTask: function(list) {
+      if (this.messages[list.id] === undefined) {
+        alert('You cannot add empty task. Please try again')
+      } else {
+        var data = new FormData
+        data.append('card[list_id]', list.id)
+        data.append('card[name]', this.messages[list.id])
+
+        Rails.ajax({
+          url: '/cards',
+          type: 'POST',
+          data: data,
+          dataType: 'json',
+          success: (data)  => {
+            const index = this.lists.findIndex(item => item.id === list.id)
+            
+            this.lists[index].cards.push(data)
+            this.messages[list.id] = undefined
+          }
+        })
+
+      }
+    },
+  }
 }
 </script>
 
